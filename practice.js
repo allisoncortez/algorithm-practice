49db5f47a6492a00ae718694eeb89f216a2d4faa
allisoncortez <allizetorc@gmail.com>
2021-01-31 21:43:01 -0600

    add pyramid, recursive solution

--- practice.js
+++ practice.js
@@ -133,3 +133,29 @@ function chunk(array, size) {
 
     return emptyArr;
 }
+
+//pyramids, recursive solution
+function pyramid(n, row = 0, level = '') {
+    //base case
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    const midpoint = Math.floor(2 * n - 1 / 2);
+    let add;
+    //we determine which column we're operating on by looking at the length of our level string
+    // && makes sure we're still to the left of this bound
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    //after determining what character add will be, we'll insert this add this new variable into our recursive solution
+    pyramid(n,row,level + add);
+
+}
