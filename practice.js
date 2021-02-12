7142681619d0e3da054ea47f497145a21bf1fccb
allisoncortez <allizetorc@gmail.com>
2021-02-12 13:18:59 -0600

    setup first 3 questions

--- practice.js
+++ practice.js
@@ -1,67 +1,42 @@
-//capitalize a string
-function capitalize(str) {
-    let result = str[0].toUpperCase();
+1.
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
 
-    for(let i = 1; i < str.length; i++) {
-        if(str[i - 1] === ' ') {
-            result += str[i].toUpperCase()
-        } else {
-            result += str[i]
-        }
-    }
+//iterative solution
+// function vowels(str){
 
-    return result;
-}
-
-//fizzbuzz
-function fizzBuzz(n) {
-    //need to iterate through the numbers.. > a for loop
-    for ( let i = 1; i <= n; i++) {
-        //is the number % of 3 and 5?
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('fizzbuzz');
-        } else if (i % 3 === 0) {
-            console.log('fizz');
-        } else if (i % 5 === 0) {
-            console.log('buzz')
-        } else {
-            console.log(i);
-        }
-    }
-}
-
-//palindrome problem
-//USING ARRAY.EVERY
-// function palindrome(str) {
-
-//     return str.split('').every((char, i) => {
-//         //we're minusing 1 because our index starts at 0
-//         return char === str[str.length - i - 1];
-//     });
 // }
 
-//MAIN SOLUTION
-function palindrome(str) {
-    const newstr = str.split('').reverse().join('')
+2.
+// --- Directions
+// Write a function that accepts an integer N
+// and returns a NxN spiral matrix.
+// --- Examples
+//   matrix(2)
+//     [[1, 2],
+//     [4, 3]]
+//   matrix(3)
+//     [[1, 2, 3],
+//     [8, 9, 4],
+//     [7, 6, 5]]
+//  matrix(4)
+//     [[1,   2,  3, 4],
+//     [12, 13, 14, 5],
+//     [11, 16, 15, 6],
+//     [10,  9,  8, 7]]
 
-    if (str === newstr ) {
-        return true
-    }
+3.
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
 
-        // more straightforward way of the if statement
-        // return str === newstr;
-}
-
-//working with chunks
-function chunk(array, size) {
-    //slice method... leters.slice(0,3) 3 is the element where we will stop, 0 is where we start the slice
-    const emptyArr = [];
-    let index = 0;
-
-    while (index < array.length) {
-        emptyArr.push(array.slice(index, index + size));
-        index += size;
-    }
-
-    return emptyArr;
-}
